31d692f8918616b8f20752844f976d32f568ddf2
Rodrigo Vilela <rpiresvilela@gmail.com>
2020-05-07 19:34:43 -0300

    Criação do formulário de cadastro

--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Título da Página</a>
+  <a class="navbar-brand" href="#">Nome da loja</a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+    <div class="navbar-nav ml-auto">
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Cadastro</a>
       <a class="nav-item nav-link" href="#">Login</a>
